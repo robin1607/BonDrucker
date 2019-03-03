a076b04ef84c6026436113846701c441e3834643
Robin Klos <robin-klos@web.de>
2019-03-03 19:11:32 +0100

    Added class to handle xmls with function to save / added class to handle exceptions

---
+++ BonDrucker/XMLHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace BonDrucker
+{
+    static class XMLHandler
+    {
+        static void save<T>(T objectToSave,string fileName)
+        {
+            if (objectToSave == null) { return; }
+
+            try
+            {
+                XmlDocument xmlDocument = new XmlDocument();
+                XmlSerializer serializer = new XmlSerializer(objectToSave.GetType());
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    serializer.Serialize(stream, objectToSave);
+                    stream.Position = 0;
+                    xmlDocument.Load(stream);
+                    xmlDocument.Save(fileName);
+                    stream.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                ExceptionHandler.Log(ex);
+            }
+
+
+        }
+
+        static void update(string fileName, string filePath)
+        {
+
+
+
+        }
+
+        static string read(string fileName, string filePath)
+        {
+
+
+
+        }
+    }
+}

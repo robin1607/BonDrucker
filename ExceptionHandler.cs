91aafd82634d8ffded8ae33bd9f33e10b49f8026
robin1607 <29478410+robin1607@users.noreply.github.com>
2019-03-09 15:31:45 +0100

    Add files via upload

---
+++ ExceptionHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BonDrucker
+{
+    class ExceptionHandler
+    {
+        static string filePath = @"C:\Users\Robin Klos\Documents\Visual Studio 2015\Projects\BonDrucker\BonDrucker\Error.txt";
+
+        public static void Log(Exception ex)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                writer.WriteLine("-----------------------------------------------------------------------------");
+                writer.WriteLine("Date : " + DateTime.Now.ToString());
+                writer.WriteLine();
+
+                while (ex != null)
+                {
+                    writer.WriteLine(ex.GetType().FullName);
+                    writer.WriteLine("Message : " + ex.Message);
+                    writer.WriteLine("StackTrace : " + ex.StackTrace);
+
+                    ex = ex.InnerException;
+                }
+            }
+        }
+    }
+}
